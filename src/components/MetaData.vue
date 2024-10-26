acc9cb0664a5cb1b91df932fdabdeec2f4efd67c
Vedant Gad <92710307+Vedantgad2509@users.noreply.github.com>
2024-10-26 23:51:40 +0530

    extracting url metadat

--- src/components/MetaData.vue
+++ src/components/MetaData.vue
@@ -8,13 +8,21 @@
       <h2>Metadata</h2>
       <p><strong>Title:</strong> {{ metadata.title }}</p>
       <!-- <p><strong>Description:</strong> {{ metadata.description }}</p> -->
-      <p><strong>Channel Name:</strong> {{ metadata.profileUsername }}</p>
-
+      <p v-if="metadata.profileUsername"><strong>Channel Name:</strong> {{ metadata.profileUsername }}</p>
+        <p v-if="metadata.site_name"><strong>Site Name:</strong>{{ metadata.site_name }}</p>
+        <p v-if="metadata.type"><strong>Type:</strong>{{ metadata.type }}</p>
       <img
         :src="metadata.imageSecureUrl"
         alt="Metadata Image"
         v-if="metadata.imageSecureUrl"
       />
+      <img
+      :src="metadata.image"
+      alt="Metadat Image"
+      v-if="metadata.image"
+      />
+      
+
     </div>
   </div>
 </template>
